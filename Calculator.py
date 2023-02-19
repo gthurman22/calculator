22b74295fcc18af6212949fcee3771d7055e22d6
gthurman22 <120196340+gthurman22@users.noreply.github.com>
2023-02-18 20:38:53 -0500

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[22]:
-
-
 num1 = float(input("Enter the first number: "))
 op = input("Enter Operator: ")
 num2 = float(input("Enter the second number: "))
@@ -20,10 +14,3 @@ else:
                 print("Invalid operation")
         #What we're doing is getting the input from user, and using if statements to see if they want addition, division, etc.
         #operator = +, -, *, /
-
-
-# In[ ]:
-
-
-
-
